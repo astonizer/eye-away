115e3088f571c13ece10196648826a138186699c
Astel <astelthottankara@gmail.com>
2020-09-23 12:20:54 +0530

    Added comments

--- background.js
+++ background.js
@@ -1,18 +1,23 @@
 const name = "eye-break";
-const delayInMinutes = 0.1;
-const periodInMinutes = delayInMinutes;
+let delayInMinutes = 0.1;
+let periodInMinutes = delayInMinutes;
 
+// Process to run on alarm
 function processAlarm(alarm) {
     if(alarm) {
         console.log(alarm);
+    } else {
+        console.log("No alarm detected")
     }
 }
 
+// Creating alarm on starting the browser
 browser.alarms.create(name, {
     delayInMinutes,
     periodInMinutes
 });
 
+// Handling case when the alarm goes off
 browser.alarms.onAlarm.addListener((alarm) => {
-    console.log("Alarm rang", alarm);
+    processAlarm(alarm);
 });
